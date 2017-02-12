2f8618682135ef4084d3ec8be11b522605d24777
Guillaume Rio <guillaume.rio@sistema-strategy.com>
2017-02-12 17:41:56 +0100

    Ajout de log4j pour les logs, force utilisation de java 8
    	- Exclusion des fichiers de log pour git via gitignore
    	- Ajout des loggers dans les classes
    	- Création du fichier de paramétrage des logs
    	- modification du pom pour les dépendances à log4j

--- src/main/java/com/thomas/cof/service/PersonnageService.java
+++ src/main/java/com/thomas/cof/service/PersonnageService.java
@@ -2,11 +2,14 @@ package com.thomas.cof.service;
 
 import com.thomas.cof.dao.PersonnageDAO;
 import com.thomas.cof.domaine.Personnage;
+import org.apache.log4j.Logger;
 
 /**
  * Created by TS on 11/02/2017.
  */
 public class PersonnageService {
+
+    private final static Logger logger = Logger.getLogger(PersonnageService.class);
 
     /**
      * méthode pour écrire un personnage en BD
@@ -26,6 +29,8 @@ public class PersonnageService {
 
         PersonnageDAO persoDao = new PersonnageDAO();
         persoDao.ecrireUnPersonnage(perso);
+
+        logger.debug("sortie du service");
     }
 
 
